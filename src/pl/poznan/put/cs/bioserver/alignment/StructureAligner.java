52e109206edc3a765d9d087573da4be8af0234b2
Tomasz Żok <tomasz.zok@gmail.com>
2012-08-28 13:12:17 +0200

    Updated code for structure alignment

--- src/pl/poznan/put/cs/bioserver/alignment/StructureAligner.java
+++ src/pl/poznan/put/cs/bioserver/alignment/StructureAligner.java
@@ -20,100 +20,100 @@ import pl.poznan.put.cs.bioserver.gui.PdbManager;
 import pl.poznan.put.cs.bioserver.helper.Helper;
 
 public class StructureAligner {
-    private static Logger LOGGER = Logger.getLogger(StructureAligner.class);
+	private static Logger LOGGER = Logger.getLogger(StructureAligner.class);
 
-    public static Structure[] align(Structure s1, Structure s2)
-            throws StructureException {
-        LOGGER.info("Aligning the following structures: " + s1.getName()
-                + " and " + s2.getName());
-        Set<String> c1 = new TreeSet<>();
-        Set<String> c2 = new TreeSet<>();
-        for (Chain c : s1.getChains())
-            c1.add(c.getChainID());
-        for (Chain c : s2.getChains())
-            c2.add(c.getChainID());
-        c1.retainAll(c2);
-        if (LOGGER.isDebugEnabled()) {
-            StringBuilder builder = new StringBuilder();
-            for (String chainName : c1) {
-                builder.append(chainName);
-                builder.append(' ');
-            }
-            LOGGER.debug("The following chain names are common for both "
-                    + "structures: " + builder.toString());
-        }
+	public static Structure[] align(Structure s1, Structure s2)
+			throws StructureException {
+		LOGGER.info("Aligning the following structures: " + s1.getPDBCode()
+				+ " and " + s2.getPDBCode());
+		Set<String> c1 = new TreeSet<>();
+		Set<String> c2 = new TreeSet<>();
+		for (Chain c : s1.getChains())
+			c1.add(c.getChainID());
+		for (Chain c : s2.getChains())
+			c2.add(c.getChainID());
+		c1.retainAll(c2);
+		if (LOGGER.isDebugEnabled()) {
+			StringBuilder builder = new StringBuilder();
+			for (String chainName : c1) {
+				builder.append(chainName);
+				builder.append(' ');
+			}
+			LOGGER.debug("The following chain names are common for both "
+					+ "structures: " + builder.toString());
+		}
 
-        Chain[][] chains = new Chain[c1.size()][];
-        int i = 0;
-        for (String id : c1) {
-            chains[i++] = align(s1.getChainByPDB(id), s2.getChainByPDB(id));
-            LOGGER.trace("Aligned chain: " + id);
-        }
+		Chain[][] chains = new Chain[c1.size()][];
+		int i = 0;
+		for (String id : c1) {
+			chains[i++] = align(s1.getChainByPDB(id), s2.getChainByPDB(id));
+			LOGGER.trace("Aligned chain: " + id);
+		}
 
-        Structure[] structures = new Structure[] { s1.clone(), s2.clone(),
-                s1.clone(), s2.clone() };
-        for (i = 0; i < 4; i++) {
-            Vector<Chain> vector = new Vector<>();
-            for (int j = 0; j < chains.length; j++)
-                vector.add(chains[j][i]);
-            structures[i].setChains(vector);
-        }
-        return structures;
-    }
+		Structure[] structures = new Structure[] { s1.clone(), s2.clone(),
+				s1.clone(), s2.clone() };
+		for (i = 0; i < 4; i++) {
+			Vector<Chain> vector = new Vector<>();
+			for (int j = 0; j < chains.length; j++)
+				vector.add(chains[j][i]);
+			structures[i].setChains(vector);
+		}
+		return structures;
+	}
 
-    public static Chain[] align(Chain c1, Chain c2) throws StructureException {
-        StructurePairAligner aligner = new StructurePairAligner();
-        if (Helper.isNucleicAcid(c1)) {
-            StrucAligParameters parameters = new StrucAligParameters();
-            parameters.setUsedAtomNames(new String[] { " C1'", " C2 ", " C2'",
-                    " C3'", " C4 ", " C4'", " C5 ", " C5'", " C6 ", " N1 ",
-                    " N3 ", " O2'", " O3'", " O4'", " O5'", " OP1", " OP2",
-                    " P  " });
-            aligner.setParams(parameters);
-        }
+	public static Chain[] align(Chain c1, Chain c2) throws StructureException {
+		StructurePairAligner aligner = new StructurePairAligner();
+		if (Helper.isNucleicAcid(c1)) {
+			StrucAligParameters parameters = new StrucAligParameters();
+			parameters.setUsedAtomNames(new String[] { " C1'", " C2 ", " C2'",
+					" C3'", " C4 ", " C4'", " C5 ", " C5'", " C6 ", " N1 ",
+					" N3 ", " O2'", " O3'", " O4'", " O5'", " OP1", " OP2",
+					" P  " });
+			aligner.setParams(parameters);
+		}
 
-        StructureImpl s1 = new StructureImpl(c1);
-        StructureImpl s2 = new StructureImpl(c2);
-        aligner.align(s1, s2);
-        AlternativeAlignment alignment = aligner.getAlignments()[0];
-        Structure structure = alignment.getAlignedStructure(s1, s2);
-        c1 = structure.getModel(0).get(0);
-        c2 = structure.getModel(1).get(0);
+		StructureImpl s1 = new StructureImpl(c1);
+		StructureImpl s2 = new StructureImpl(c2);
+		aligner.align(s1, s2);
+		AlternativeAlignment alignment = aligner.getAlignments()[0];
+		Structure structure = alignment.getAlignedStructure(s1, s2);
+		c1 = structure.getModel(0).get(0);
+		c2 = structure.getModel(1).get(0);
 
-        Chain c3 = (Chain) c1.clone();
-        Chain c4 = (Chain) c2.clone();
-        String[][] residues = new String[][] { alignment.getPDBresnum1(),
-                alignment.getPDBresnum2() };
-        c3.setAtomGroups(filterGroups(c1, residues[0]));
-        c4.setAtomGroups(filterGroups(c2, residues[1]));
-        PdbManager.putAlignmentInfo(new Chain[] { c1, c2 }, residues);
+		Chain c3 = (Chain) c1.clone();
+		Chain c4 = (Chain) c2.clone();
+		String[][] residues = new String[][] { alignment.getPDBresnum1(),
+				alignment.getPDBresnum2() };
+		c3.setAtomGroups(filterGroups(c1, residues[0]));
+		c4.setAtomGroups(filterGroups(c2, residues[1]));
+		PdbManager.putAlignmentInfo(new Chain[] { c1, c2 }, residues);
 
-        if (LOGGER.isDebugEnabled()) {
-            String[] numerals = new String[] { "1st", "2nd" };
-            for (int i = 0; i < 2; i++) {
-                StringBuilder builder = new StringBuilder();
-                for (String residueNumber : residues[i]) {
-                    builder.append(residueNumber);
-                    builder.append('\t');
-                }
-                LOGGER.debug("Residues aligned from " + numerals[i]
-                        + " structure: " + builder.toString());
-            }
-        }
+		if (LOGGER.isTraceEnabled()) {
+			String[] numerals = new String[] { "1st", "2nd" };
+			for (int i = 0; i < 2; i++) {
+				StringBuilder builder = new StringBuilder();
+				for (String residueNumber : residues[i]) {
+					builder.append(residueNumber);
+					builder.append('\t');
+				}
+				LOGGER.trace("Residues aligned from " + numerals[i]
+						+ " structure: " + builder.toString());
+			}
+		}
 
-        return new Chain[] { c1, c2, c3, c4 };
-    }
+		return new Chain[] { c1, c2, c3, c4 };
+	}
 
-    private static List<Group> filterGroups(Chain c1, String[] indices) {
-        Set<Integer> set = new HashSet<>();
-        for (String s : indices)
-            set.add(Integer.valueOf(s.split(":")[0]));
+	private static List<Group> filterGroups(Chain c1, String[] indices) {
+		Set<Integer> set = new HashSet<>();
+		for (String s : indices)
+			set.add(Integer.valueOf(s.split(":")[0]));
 
-        List<Group> list = new Vector<>();
-        for (Group g : c1.getAtomGroups()) {
-            if (set.contains(g.getResidueNumber().getSeqNum()))
-                list.add(g);
-        }
-        return list;
-    }
+		List<Group> list = new Vector<>();
+		for (Group g : c1.getAtomGroups()) {
+			if (set.contains(g.getResidueNumber().getSeqNum()))
+				list.add(g);
+		}
+		return list;
+	}
 }
--- src/pl/poznan/put/cs/bioserver/gui/PdbManager.java
+++ src/pl/poznan/put/cs/bioserver/gui/PdbManager.java
@@ -7,103 +7,114 @@ import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Vector;
 
+import org.apache.log4j.Logger;
 import org.biojava.bio.structure.Chain;
 import org.biojava.bio.structure.Group;
 import org.biojava.bio.structure.Structure;
 import org.biojava.bio.structure.io.PDBFileReader;
 
 public class PdbManager {
-    private final HashMap<String, Structure> mapStructure;
-    private final HashMap<String, String> nameMap;
-    private final static HashMap<Integer, HashMap<Group, Group>> mapAlignment = new HashMap<>();
+	private final HashMap<String, Structure> mapStructure;
+	private final HashMap<String, String> nameMap;
+	private final static HashMap<Integer, HashMap<Group, Group>> mapAlignment = new HashMap<>();
+	private final static Logger LOGGER = Logger.getLogger(PdbManager.class);
 
-    public PdbManager() {
-        mapStructure = new HashMap<>();
-        nameMap = new HashMap<>();
-    }
+	public PdbManager() {
+		mapStructure = new HashMap<>();
+		nameMap = new HashMap<>();
+	}
 
-    public boolean addStructure(String path) {
-        try {
-            if (mapStructure.containsKey(path))
-                return true;
+	public boolean addStructure(String path) {
+		try {
+			if (mapStructure.containsKey(path))
+				return true;
 
-            Structure structure = new PDBFileReader().getStructure(path);
-            if (structure == null || structure.size() == 0)
-                throw new IOException();
+			Structure structure = new PDBFileReader().getStructure(path);
+			if (structure == null || structure.size() == 0)
+				throw new IOException();
 
-            String name = structure.getPDBCode();
-            if (name == null || name.trim().equals("")) {
-                name = new File(path).getName();
-                structure.setPDBCode(name);
-            }
+			String name = structure.getPDBCode();
+			if (name == null || name.trim().equals("")) {
+				name = new File(path).getName();
+				structure.setPDBCode(name);
+			}
 
-            mapStructure.put(path, structure);
-            nameMap.put(path, name);
-            return true;
-        } catch (IOException e) {
-            return false;
-        }
-    }
+			mapStructure.put(path, structure);
+			nameMap.put(path, name);
+			return true;
+		} catch (IOException e) {
+			return false;
+		}
+	}
 
-    public String[] getNames(Vector<String> elements) {
-        Vector<String> vector = new Vector<>();
-        for (String element : elements) {
-            String name = nameMap.get(element);
-            vector.add(name);
-        }
-        return vector.toArray(new String[vector.size()]);
-    }
+	public String[] getNames(Vector<String> elements) {
+		Vector<String> vector = new Vector<>();
+		for (String element : elements) {
+			String name = nameMap.get(element);
+			vector.add(name);
+		}
+		return vector.toArray(new String[vector.size()]);
+	}
 
-    public Structure[] getStructures(Enumeration<?> elements) {
-        Vector<String> vector = new Vector<>();
-        while (elements.hasMoreElements())
-            vector.add((String) elements.nextElement());
-        return getStructures(vector);
-    }
+	public Structure[] getStructures(Enumeration<?> elements) {
+		Vector<String> vector = new Vector<>();
+		while (elements.hasMoreElements())
+			vector.add((String) elements.nextElement());
+		return getStructures(vector);
+	}
 
-    public Structure[] getStructures(Iterable<String> elements) {
-        Vector<Structure> vector = new Vector<>();
-        for (String element : elements) {
-            Structure structure = mapStructure.get(element);
-            vector.add(structure);
-        }
-        return vector.toArray(new Structure[vector.size()]);
-    }
+	public Structure[] getStructures(Iterable<String> elements) {
+		Vector<Structure> vector = new Vector<>();
+		for (String element : elements) {
+			Structure structure = mapStructure.get(element);
+			vector.add(structure);
+		}
+		return vector.toArray(new Structure[vector.size()]);
+	}
 
-    public Structure[] getStructures(String[] elements) {
-        return getStructures(Arrays.asList(elements));
-    }
+	public Structure[] getStructures(String[] elements) {
+		return getStructures(Arrays.asList(elements));
+	}
 
-    public static void putAlignmentInfo(Chain[] original,
-            String[][] residuesMapping) {
-        HashMap<Group, Group> map = new HashMap<>();
-        for (int i = 0; i < residuesMapping[0].length; i++) {
-            int[] indices = new int[2];
-            for (int j = 0; j < 2; j++) {
-                String residue = residuesMapping[j][i].split(":")[0];
-                indices[j] = Integer.valueOf(residue);
-            }
+	public static void putAlignmentInfo(Chain[] original,
+			String[][] residuesMapping) {
+		HashMap<Group, Group> map = new HashMap<>();
+		for (int i = 0; i < residuesMapping[0].length; i++) {
+			int[] indices = new int[2];
+			for (int j = 0; j < 2; j++) {
+				String residue = residuesMapping[j][i].split(":")[0];
+				indices[j] = Integer.valueOf(residue);
+			}
 
-            Group[] groups = new Group[2];
-            for (int j = 0; j < 2; j++)
-                groups[j] = original[j].getAtomGroup(indices[j]);
-            map.put(groups[0], groups[1]);
-        }
-        mapAlignment.put(original.hashCode(), map);
-    }
+			Group[] groups = new Group[2];
+			for (int j = 0; j < 2; j++)
+				groups[j] = original[j].getAtomGroup(indices[j]);
+			map.put(groups[0], groups[1]);
+		}
+		int hashCode = original.hashCode(); // FIXME: hashCode nie działa!
+		LOGGER.debug("Hash code of the new alignment info: " + hashCode);
+		mapAlignment.put(hashCode, map);
+	}
 
-    public static boolean isAlignmentInfo(Chain[] chains) {
-        Chain[] inverted = new Chain[] { chains[1], chains[0] };
-        return mapAlignment.containsKey(chains.hashCode())
-                || mapAlignment.containsKey(inverted.hashCode());
-    }
+	public static boolean isAlignmentInfo(Chain[] chains) {
+		Chain[] inverted = new Chain[] { chains[1], chains[0] };
+		int[] hashCodes = new int[] { chains.hashCode(), inverted.hashCode() };
+		LOGGER.debug("Checking for hash codes: " + hashCodes[0] + " and "
+				+ hashCodes[1]);
 
-    public static HashMap<Group, Group> getAlignmentInfo(Chain[] chains) {
-        HashMap<Group, Group> map = mapAlignment.get(chains.hashCode());
-        if (map == null) {
-            Chain[] inverted = new Chain[] { chains[1], chains[0] };
-            map = mapAlignment.get(inverted.hashCode());
-        }
-        return map;
-    }
+		return mapAlignment.containsKey(hashCodes[0])
+				|| mapAlignment.containsKey(hashCodes[1]);
+	}
+
+	public static HashMap<Group, Group> getAlignmentInfo(Chain[] chains) {
+		Chain[] inverted = new Chain[] { chains[1], chains[0] };
+		int[] hashCodes = new int[] { chains.hashCode(), inverted.hashCode() };
+		LOGGER.debug("Trying to get hash codes: " + hashCodes[0] + " and "
+				+ hashCodes[1]);
+
+		HashMap<Group, Group> map = mapAlignment.get(hashCodes[0]);
+		if (map == null)
+			map = mapAlignment.get(hashCodes[1]);
+		return map;
+	}
 }
