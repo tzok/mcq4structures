23c945d8d2f9da76cdb21575daaaadae0ef07af9
Tomasz Zok <Tomasz.Zok@cs.put.poznan.pl>
2012-11-06 09:58:44 +0100

    Update of benchmarking script

--- benchmark.sh
+++ benchmark.sh
@@ -1,7 +1,14 @@
 #! /bin/bash
-rm rmsd-time.dat
-for i in $(seq 100)
+for alg in mcq rmsd
 do
-    args=$(yes ~/pdb/1EHZ.pdb | head -n $i | tr '\n' ' ')
-    /usr/bin/time -f %e -o rmsd-time.dat --append ./rmsd.sh "$args" &> /dev/null
+    rm $alg-time-*.dat 2> /dev/null
+    for j in $(seq 10)
+    do
+        echo $alg $j
+        for i in $(seq 100)
+        do
+            args=$(yes ~/pdb/1EHZ.pdb | head -n $i | tr '\n' ' ')
+            /usr/bin/time -f %e -o $alg-time-$(printf '%02d' $j).dat --append ./$alg.sh "$args" &> /dev/null
+        done
+    done
 done
