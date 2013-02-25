89e47bb19dcb88e82abef013be6f540ee34b746b
Tomasz Żok <Tomasz.Zok@cs.put.poznan.pl>
2013-02-25 22:05:49 +0100

    New layout for chain choosing dialog

--- src/pl/poznan/put/cs/bioserver/gui/DialogChains.java
+++ src/pl/poznan/put/cs/bioserver/gui/DialogChains.java
@@ -25,7 +25,6 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.ListCellRenderer;
-import javax.swing.border.Border;
 
 import org.biojava.bio.structure.Chain;
 import org.biojava.bio.structure.Structure;
@@ -35,6 +34,103 @@ import pl.poznan.put.cs.bioserver.helper.Helper;
 import pl.poznan.put.cs.bioserver.helper.StructureManager;
 
 final class DialogChains extends JDialog {
+    private class PanelChains extends JPanel {
+        private static final long serialVersionUID = 1L;
+
+        private DefaultComboBoxModel<File> model = new DefaultComboBoxModel<>();
+        private JComboBox<File> combo = new JComboBox<>(model);
+        private JPanel[] panels = new JPanel[] { new JPanel(), new JPanel() };
+
+        public PanelChains() {
+            super(new BorderLayout());
+
+            panels[0].setLayout(new BoxLayout(panels[0], BoxLayout.Y_AXIS));
+            panels[1].setLayout(new BoxLayout(panels[1], BoxLayout.Y_AXIS));
+
+            JPanel panel = new JPanel(new BorderLayout());
+            panel.add(new JLabel("Select structure"), BorderLayout.NORTH);
+            panel.add(combo, BorderLayout.CENTER);
+            add(panel, BorderLayout.NORTH);
+
+            panel = new JPanel(new GridLayout(1, 2));
+            panel.setBorder(BorderFactory.createTitledBorder("Select chain(s)"));
+            panel.add(panels[0]);
+            panel.add(panels[1]);
+            add(new JScrollPane(panel), BorderLayout.CENTER);
+
+            final ListCellRenderer<? super File> renderer = combo.getRenderer();
+            combo.setRenderer(new ListCellRenderer<File>() {
+                @Override
+                public Component getListCellRendererComponent(
+                        JList<? extends File> list, File value, int index,
+                        boolean isSelected, boolean cellHasFocus) {
+                    JLabel label = (JLabel) renderer
+                            .getListCellRendererComponent(list, value, index,
+                                    isSelected, cellHasFocus);
+                    if (value != null) {
+                        label.setText(StructureManager.getName(value));
+                    }
+                    return label;
+                }
+            });
+
+            combo.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    File file = (File) combo.getSelectedItem();
+                    if (file == null) {
+                        return;
+                    }
+                    Structure structure = StructureManager.getStructure(file);
+                    if (structure == null) {
+                        return;
+                    }
+
+                    panels[0].removeAll();
+                    panels[1].removeAll();
+                    panels[0].add(new JLabel("RNAs:"));
+                    panels[1].add(new JLabel("Proteins:"));
+                    for (Chain chain : structure.getChains()) {
+                        JCheckBox checkBox = new JCheckBox(chain.getChainID());
+                        int index = Helper.isNucleicAcid(chain) ? 0 : 1;
+                        panels[index].add(checkBox);
+                    }
+                    panels[0].updateUI();
+                    panels[1].updateUI();
+                }
+            });
+        }
+
+        public Chain[] getSelectedChains() {
+            File file = (File) combo.getSelectedItem();
+            if (file == null) {
+                return null;
+            }
+            Structure structure = StructureManager.getStructure(file);
+            if (structure == null) {
+                return null;
+            }
+
+            List<Chain> list = new ArrayList<>();
+            for (JPanel panel : panels) {
+                for (Component component : panel.getComponents()) {
+                    if (component instanceof JCheckBox
+                            && ((JCheckBox) component).isSelected()) {
+                        String chainId = ((JCheckBox) component).getText();
+                        try {
+                            list.add(structure.getChainByPDB(chainId));
+                        } catch (StructureException e) {
+                            JOptionPane.showMessageDialog(DialogChains.this,
+                                    e.getMessage(), "Error",
+                                    JOptionPane.ERROR_MESSAGE);
+                        }
+                    }
+                }
+            }
+            return list.toArray(new Chain[list.size()]);
+        }
+    }
+
     private static final long serialVersionUID = 1L;
     public static final int CANCEL = 0;
     public static final int OK = 1;
@@ -49,66 +145,29 @@ final class DialogChains extends JDialog {
     }
 
     private int chosenOption;
-    private File[] selectedStructures;
-    private Chain[][] selectedChains;
-
-    private DefaultComboBoxModel<File> modelLeft = new DefaultComboBoxModel<>();
-    private JComboBox<File> comboLeft = new JComboBox<>(modelLeft);
-    private JPanel panelChainsLeft = new JPanel();
-    private JButton buttonRNAsLeft = new JButton("Select RNA chains");
-    private JButton buttonProteinsLeft = new JButton("Select protein chains");
-
-    private DefaultComboBoxModel<File> modelRight = new DefaultComboBoxModel<>();
-    private JComboBox<File> comboRight = new JComboBox<>(modelRight);
-    private JPanel panelChainsRight = new JPanel();
-    private JButton buttonRNAsRight = new JButton("Select RNA chains");
-    private JButton buttonProteinsRight = new JButton("Select protein chains");
+    private File[] structures;
+    private Chain[][] chains;
+    private PanelChains[] panelsChains;
 
     private DialogChains(Frame owner) {
         super(owner, true);
         setLayout(new BorderLayout());
         setTitle("MCQ4Structures: structure & chain selection");
 
-        Border border = BorderFactory.createTitledBorder("Select chain(s)");
+        panelsChains = new PanelChains[2];
+        panelsChains[0] = new PanelChains();
+        panelsChains[1] = new PanelChains();
 
-        JPanel panelLeft = new JPanel(new BorderLayout());
-        JPanel panel = new JPanel(new BorderLayout());
-        panel.add(new JLabel("Select structure"), BorderLayout.NORTH);
-        panel.add(comboLeft, BorderLayout.CENTER);
-        panelLeft.add(panel, BorderLayout.NORTH);
-        BoxLayout layout = new BoxLayout(panelChainsLeft, BoxLayout.Y_AXIS);
-        panelChainsLeft.setLayout(layout);
-        panelChainsLeft.setBorder(border);
-        panelLeft.add(new JScrollPane(panelChainsLeft), BorderLayout.CENTER);
-        panel = new JPanel();
-        panel.add(buttonRNAsLeft);
-        panel.add(buttonProteinsLeft);
-        panelLeft.add(panel, BorderLayout.SOUTH);
+        JPanel panelBoth = new JPanel(new GridLayout(1, 2));
+        panelBoth.add(panelsChains[0]);
+        panelBoth.add(panelsChains[1]);
 
-        JPanel panelRight = new JPanel(new BorderLayout());
-        panel = new JPanel(new BorderLayout());
-        panel.add(new JLabel("Select structure"), BorderLayout.NORTH);
-        panel.add(comboRight, BorderLayout.CENTER);
-        panelRight.add(panel, BorderLayout.NORTH);
-        layout = new BoxLayout(panelChainsRight, BoxLayout.Y_AXIS);
-        panelChainsRight.setLayout(layout);
-        panelChainsRight.setBorder(border);
-        panelRight.add(new JScrollPane(panelChainsRight), BorderLayout.CENTER);
-        panel = new JPanel();
-        panel.add(buttonRNAsRight);
-        panel.add(buttonProteinsRight);
-        panelRight.add(panel, BorderLayout.SOUTH);
-
-        panel = new JPanel();
-        panel.setLayout(new GridLayout(1, 2));
-        panel.add(panelLeft);
-        panel.add(panelRight);
-        add(panel, BorderLayout.CENTER);
         JButton buttonOk = new JButton("OK");
         JButton buttonCancel = new JButton("Cancel");
-        panel = new JPanel();
+        JPanel panel = new JPanel();
         panel.add(buttonOk);
         panel.add(buttonCancel);
+        add(panelBoth, BorderLayout.CENTER);
         add(panel, BorderLayout.SOUTH);
 
         int width = 640;
@@ -119,138 +178,25 @@ final class DialogChains extends JDialog {
         setSize(width, height);
         setLocation(x / 2, y / 2);
 
-        ActionListener selectChainsListener = new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent arg0) {
-                if (StructureManager.getAllStructures().size() == 0) {
-                    return;
-                }
-
-                JPanel panelReference;
-                Structure structure;
-                boolean isRna;
-
-                Object source = arg0.getSource();
-                if (source.equals(buttonRNAsLeft)) {
-                    panelReference = panelChainsLeft;
-                    structure = StructureManager.getStructure((File) comboLeft
-                            .getSelectedItem());
-                    isRna = true;
-                } else if (source.equals(buttonProteinsLeft)) {
-                    panelReference = panelChainsLeft;
-                    structure = StructureManager.getStructure((File) comboLeft
-                            .getSelectedItem());
-                    isRna = false;
-                } else if (source.equals(buttonRNAsRight)) {
-                    panelReference = panelChainsRight;
-                    structure = StructureManager.getStructure((File) comboRight
-                            .getSelectedItem());
-                    isRna = true;
-                } else { // source.equals(buttonSelectProteinsRight)
-                    panelReference = panelChainsRight;
-                    structure = StructureManager.getStructure((File) comboRight
-                            .getSelectedItem());
-                    isRna = false;
-                }
-
-                for (Component component : panelReference.getComponents()) {
-                    if (component instanceof JCheckBox) {
-                        String chainId = ((JCheckBox) component).getText();
-                        try {
-                            Chain chain = structure.getChainByPDB(chainId);
-                            if (Helper.isNucleicAcid(chain) == isRna) {
-                                ((JCheckBox) component).setSelected(true);
-                            }
-                        } catch (StructureException e) {
-                            JOptionPane.showMessageDialog(DialogChains.this,
-                                    e.getMessage(), "Error",
-                                    JOptionPane.ERROR_MESSAGE);
-                        }
-                    }
-                }
-            }
-        };
-        buttonRNAsLeft.addActionListener(selectChainsListener);
-        buttonProteinsLeft.addActionListener(selectChainsListener);
-        buttonRNAsRight.addActionListener(selectChainsListener);
-        buttonProteinsRight.addActionListener(selectChainsListener);
-
-        final ListCellRenderer<? super File> renderer = comboLeft.getRenderer();
-        ListCellRenderer<File> pdbCellRenderer = new ListCellRenderer<File>() {
-            @Override
-            public Component getListCellRendererComponent(
-                    JList<? extends File> list, File value, int index,
-                    boolean isSelected, boolean cellHasFocus) {
-                JLabel label = (JLabel) renderer.getListCellRendererComponent(
-                        list, value, index, isSelected, cellHasFocus);
-                if (value != null) {
-                    label.setText(StructureManager.getName(value));
-                }
-                return label;
-            }
-        };
-        comboLeft.setRenderer(pdbCellRenderer);
-        comboRight.setRenderer(pdbCellRenderer);
-
-        ActionListener actionListenerCombo = new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                JComboBox<File> source = (JComboBox<File>) e.getSource();
-                File file = (File) source.getSelectedItem();
-                if (file == null) {
-                    return;
-                }
-                Structure structure = StructureManager.getStructure(file);
-                if (structure == null) {
-                    return;
-                }
-
-                JPanel panelReference;
-                if (source.equals(comboLeft)) {
-                    panelReference = panelChainsLeft;
-                } else {
-                    panelReference = panelChainsRight;
-                }
-
-                panelReference.removeAll();
-                for (Chain chain : structure.getChains()) {
-                    panelReference.add(new JCheckBox(chain.getChainID()));
-                }
-                panelReference.updateUI();
-            }
-        };
-        comboLeft.addActionListener(actionListenerCombo);
-        comboRight.addActionListener(actionListenerCombo);
-
         buttonOk.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent arg0) {
-                @SuppressWarnings("rawtypes")
-                JComboBox[] combos = new JComboBox[] { comboLeft, comboRight };
-                JPanel[] panels = new JPanel[] { panelChainsLeft,
-                        panelChainsRight };
+                structures = new File[2];
+                structures[0] = (File) panelsChains[0].combo.getSelectedItem();
+                structures[1] = (File) panelsChains[1].combo.getSelectedItem();
+                if (structures[0] == null || structures[1] == null) {
+                    chosenOption = DialogChains.CANCEL;
+                    dispose();
+                    return;
+                }
 
-                selectedStructures = new File[2];
-                selectedChains = new Chain[2][];
-                for (int i = 0; i < 2; i++) {
-                    List<Chain> list = new ArrayList<>();
-                    File pdb = (File) combos[i].getSelectedItem();
-                    Structure structure = StructureManager.getStructure(pdb);
-                    for (Component component : panels[i].getComponents()) {
-                        if (component instanceof JCheckBox
-                                && ((JCheckBox) component).isSelected()) {
-                            String chainId = ((JCheckBox) component).getText();
-                            try {
-                                list.add(structure.getChainByPDB(chainId));
-                            } catch (StructureException e) {
-                                JOptionPane.showMessageDialog(
-                                        DialogChains.this, e.getMessage(),
-                                        "Error", JOptionPane.ERROR_MESSAGE);
-                            }
-                        }
-                    }
-                    selectedStructures[i] = pdb;
-                    selectedChains[i] = list.toArray(new Chain[list.size()]);
+                chains = new Chain[2][];
+                chains[0] = panelsChains[0].getSelectedChains();
+                chains[1] = panelsChains[1].getSelectedChains();
+                if (chains[0] == null || chains[1] == null) {
+                    chosenOption = DialogChains.CANCEL;
+                    dispose();
+                    return;
                 }
 
                 chosenOption = DialogChains.OK;
@@ -268,19 +214,19 @@ final class DialogChains extends JDialog {
     }
 
     public Chain[][] getChains() {
-        return selectedChains;
+        return chains;
     }
 
     public File[] getFiles() {
-        return selectedStructures;
+        return structures;
     }
 
     public String getSelectionDescription() {
         StringBuilder builder = new StringBuilder();
         for (int i = 0; i < 2; i++) {
-            builder.append(StructureManager.getName(selectedStructures[i]));
+            builder.append(StructureManager.getName(structures[i]));
             builder.append('.');
-            for (Chain chain : selectedChains[i]) {
+            for (Chain chain : chains[i]) {
                 builder.append(chain.getChainID());
             }
             if (i == 0) {
@@ -291,11 +237,11 @@ final class DialogChains extends JDialog {
     }
 
     public int showDialog() {
-        modelLeft.removeAllElements();
-        modelRight.removeAllElements();
+        panelsChains[0].model.removeAllElements();
+        panelsChains[1].model.removeAllElements();
         for (File file : StructureManager.getAllStructures()) {
-            modelLeft.addElement(file);
-            modelRight.addElement(file);
+            panelsChains[0].model.addElement(file);
+            panelsChains[1].model.addElement(file);
         }
 
         chosenOption = DialogChains.CANCEL;
