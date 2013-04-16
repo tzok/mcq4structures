eef3a7af605f519d24063dd77c21c97cd3539080
Tomasz Żok <Tomasz.Zok@cs.put.poznan.pl>
2013-04-16 14:45:18 +0200

    Building environment fixed for the new library dependency

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
-classpath=lib/AAProperties-jar-with-dependencies.jar:lib/biojava3-alignment-3.0.2.jar:lib/biojava3-core-3.0.2.jar:lib/biojava3-structure-3.0.2.jar:lib/biojava3-structure-gui-3.0.2.jar:lib/diffutils-1.2.1.jar:lib/jcommon-1.0.17.jar:lib/jfreechart-1.0.14.jar:lib/JmolData.jar:lib/Jmol.jar:lib/log4j.jar:bin/
-java -cp $classpath pl.poznan.put.cs.bioserver.gui.Gui
+classpath=$(ls lib/* | tr '\n' ':')
+java -cp bin/:$classpath pl.poznan.put.cs.bioserver.gui.Gui
