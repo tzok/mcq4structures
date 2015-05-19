d14a0cffed7b2cc31673f59fabdbdf034951b6f1
Tomasz Żok <Tomasz.Zok@cs.put.poznan.pl>
2015-05-19 15:58:47 +0200

    A script to make final ZIP and TAR.GZ archives

---
+++ make-final-archive.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+#mvn clean package dependency:copy-dependencies
+fulljar=$(ls target/*-jar-with-dependencies.jar)
+normjar=${fulljar%-jar-with-dependencies.jar}.jar
+version=${normjar#target/}
+version=${version%.jar}
+
+tempdir=$(mktemp -d)
+destdir="${tempdir}/${version}"
+
+mkdir "${destdir}"
+cp -r target/dependency "${normjar}" "${destdir}"
+cat << EOF > "${destdir}/start.bat"
+java -cp "${normjar#target/};dependency/*" pl.poznan.put.gui.Gui
+EOF
+cat << EOF > "${destdir}/start.sh"
+#! /bin/bash
+java -cp "${normjar#target/}:dependency/*" pl.poznan.put.gui.Gui
+EOF
+chmod a+x "${destdir}/start.sh"
+
+(cd "${tempdir}"; tar cfz "/tmp/${version}.tar.gz" "${version}")
+(cd "${tempdir}"; zip -rq "/tmp/${version}.zip"    "${version}")
+rm -r "${tempdir}"
+
+ls -l "/tmp/${version}.tar.gz" "/tmp/${version}.zip"
