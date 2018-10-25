6bb538fc1a77850033f7f869931834e299caec8e
Tomasz Żok <Tomasz.Zok@cs.put.poznan.pl>
2018-10-25 16:02:21 +0200

    Remove mcq-core to be updated by a submodule

--- mcq-core/src/main/java/pl/poznan/put/matching/AngleDeltaIterator.java
+++
@@ -1,8 +0,0 @@
-package pl.poznan.put.matching;
-
-import java.util.Iterator;
-import pl.poznan.put.circular.Angle;
-
-public interface AngleDeltaIterator extends Iterator<Angle> {
-  // nothing new
-}
--- mcq-core/src/main/java/pl/poznan/put/matching/AngleDeltaIteratorFactory.java
+++
@@ -1,5 +0,0 @@
-package pl.poznan.put.matching;
-
-public interface AngleDeltaIteratorFactory {
-  AngleDeltaIterator createInstance(FragmentMatch fragmentMatch);
-}
